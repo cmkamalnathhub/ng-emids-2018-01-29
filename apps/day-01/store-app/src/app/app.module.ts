8f9f4ba71306f730a9116f0458f00973be2744de
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-01-30 10:04:40 +0530

    add demo project for day 1

---
+++ apps/day-01/store-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+@NgModule({
+  declarations: [AppComponent, ProductsComponent, ProductDetailComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
